a5b40f098e14c09324a6a24117a753c63ccf8532
Antonio_Aranda7 <L17390318@chetumal.tecnm.mx>
2021-06-24 22:10:21 -0500

    capital para bandera

--- src/app/info-bandera/info-bandera.component.ts
+++ src/app/info-bandera/info-bandera.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class InfoBanderaComponent implements OnInit {
   nombrePais ="México";
   imgBandera ="./mx.svg";
+  capital ="";
   constructor() { }
 
   ngOnInit(): void {
